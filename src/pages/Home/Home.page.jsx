e33026dec23d0ecd23d125a1cb245a488bd6368b
unknown <antonio.salazar.emrsn@gmail.com>
2021-07-18 01:16:10 -0500

    Removed styles and all of the content we had in the repo, installed node-sass, creating my own layout now

--- src/pages/Home/Home.page.jsx
+++ src/pages/Home/Home.page.jsx
@@ -1,23 +1,25 @@
-import React, { useRef } from 'react';
-import { Link, useHistory } from 'react-router-dom';
+import React from 'react';
 
-import { useAuth } from '../../providers/Auth';
 import './Home.styles.css';
 
-function HomePage() {
-  const history = useHistory();
-  const sectionRef = useRef(null);
-  const { authenticated, logout } = useAuth();
+import Navbar from '../../components/Navbar/Navbar.component';
 
-  function deAuthenticate(event) {
-    event.preventDefault();
-    logout();
-    history.push('/');
-  }
+function HomePage() {
+  // const history = useHistory();
+  // const sectionRef = useRef(null);
+  // const { authenticated, logout } = useAuth();
+
+  // function deAuthenticate(event) {
+  //   event.preventDefault();
+  //   logout();
+  //   history.push('/');
+  // }
 
   return (
-    <section className="homepage" ref={sectionRef}>
-      <h1>Hello stranger!</h1>
+    <section className="homepage">
+      <Navbar />
+      {/* <h1>Hello stranger!</h1>
+      
       {authenticated ? (
         <>
           <h2>Good to have you back</h2>
@@ -31,7 +33,7 @@ function HomePage() {
         </>
       ) : (
         <Link to="/login">let me in →</Link>
-      )}
+      )} */}
     </section>
   );
 }
