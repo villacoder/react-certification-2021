ab3eaa1f8bbbc22115b0f5594c6a7370000e4597
unknown <antonio.salazar.emrsn@gmail.com>
2021-07-19 14:55:27 -0500

    The navbar is almost finished, it is responsive already, it contains the hamburguer menu, the input field for searching videos and the user button

--- src/components/Navbar/Navbar.component.jsx
+++ src/components/Navbar/Navbar.component.jsx
@@ -1,10 +1,27 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
-import { FaHamburger, FaWindowClose, FaHome } from 'react-icons/fa';
+import { FaHamburger, FaWindowClose, FaHome, FaUserNinja } from 'react-icons/fa';
 import { IconContext } from 'react-icons/lib';
 import './Navbar.styles.scss';
-// import styled from 'styled-components';
+import styled from 'styled-components';
+
+const Input = styled.input`
+  background-color: #060b26;
+  border: none;
+  color: #fff;
+  margin-left: 2rem;
+  padding: 0.5rem;
+  width: 60%;
+  transition: all 0.3s ease;
+  &::placeholder {
+    color: #fff;
+  }
+  &:hover {
+    width: 100%;
+    border-bottom: 2px solid #fff;
+  }
+`;
 
 const Navbar = () => {
   const [sidebar, setSidebar] = useState(false);
@@ -13,12 +30,21 @@ const Navbar = () => {
 
   return (
     <>
-      <IconContext.Provider value={{ color: '#fff', size: '2em' }}>
+      <IconContext.Provider value={{ color: '#fff', size: '3rem' }}>
         <div className="navbar">
-          <Link to="#" className="menu-bars">
-            <FaHamburger onClick={toogleSideBar} />
-          </Link>
+          <div className="navbar__left-section">
+            <Link to="#" className="menu-bars">
+              <FaHamburger onClick={toogleSideBar} />
+            </Link>
+            <Input type="text" placeholder="search a video..." />
+          </div>
+          <div className="navbar__right-section">
+            <Link>
+              <FaUserNinja />
+            </Link>
+          </div>
         </div>
+
         <nav className={sidebar ? 'nav-menu active' : 'nav-menu'}>
           <ul className="nav-menu-items" onClick={toogleSideBar}>
             <li className="navbar-toggle">
