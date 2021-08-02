6c26caa86f27931d70ad078dda7111bcb5f1b31a
unknown <antonio.salazar.emrsn@gmail.com>
2021-08-01 23:52:59 -0500

    testing of the Layout component is ready

--- src/Tests/components/Layout.test.js
+++ src/Tests/components/Layout.test.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import Layout from '../../components/Layout/Layout.component';
+import Navbar from '../../components/Navbar/Navbar.component';
+jest.mock('../../components/Navbar/Navbar.component', () => 'Navbar')
 
 describe('Test of the Layout component', () => {
-    
-    let wrapper = shallow( < Layout />)
+    let children =  Navbar
+    let wrapper = shallow( < Layout children={children} />)
     
     test('main container should be rendered correctly', () => {
         expect(wrapper).toMatchSnapshot()
@@ -13,6 +15,11 @@ describe('Test of the Layout component', () => {
     test('main tag should contain the container className', () => {
         const mainContainer = wrapper.find('main')
         expect(mainContainer.hasClass('container')).toBe(true)
+    })
+    
+    test('Layout component should display the received props inside the main tag', () => {
+        const mainTag = wrapper.find('main').text()
+        expect(mainTag).toBe(children)
     })
     
     
