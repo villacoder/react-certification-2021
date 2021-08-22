42fc33168f828a6c6efe291e10111461db3130cd
ungeimer <hugocastillopelaez@gmail.com>
2021-08-22 12:59:45 -0500

    Mini-challenge 4 useContext space 2

--- src/components/VideoCard/VideoGrid.jsx
+++ src/components/VideoCard/VideoGrid.jsx
@@ -6,8 +6,9 @@ import VideoContext from '../../context/VideoContext';
 
 const youtube = new YouTube(process.env.REACT_APP_API_KEY_YOUTUBE);
 export const VideoGrid = () => {
-  const { passToChild, selectedVideo, setSelectedVideo, setPlayVideo } =
-  useContext(VideoContext);
+  const { passToChild, selectedVideo, setSelectedVideo, setPlayVideo } = useContext(
+    VideoContext
+    );
   const [videoList, setVideoList] = useState([]);
   const [error, setError] = useState(false);
 
