cba11c3bbd248db2ef51fcd184f94a0e2f532776
ungeimer <hugocastillopelaez@gmail.com>
2021-09-01 16:36:40 -0500

    Page title

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Helmet } from 'react-helmet'
 
 import { Principal } from './Principal';
 import './global.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+const TITLE = 'WizeTube';
 ReactDOM.render(
   <React.StrictMode>
     <Principal />
+    <Helmet>
+          <title>{ TITLE }</title>
+        </Helmet>
   </React.StrictMode>,
   document.getElementById('root')
 );
